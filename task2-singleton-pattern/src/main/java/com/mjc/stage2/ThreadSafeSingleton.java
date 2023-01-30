b65272f97d4aec0617cbf540d0d22b49ccc0270c
Roman Gulevich <r.i.gulevich@gmail.com>
2023-01-30 13:23:08 +0300

    Added ThreadSageSingleton implementation

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,5 +1,20 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
+
+    private static volatile ThreadSafeSingleton safeSingleton;
+
+    private ThreadSafeSingleton() {
+    }
+
+    public static ThreadSafeSingleton getInstance() {
+        if (safeSingleton == null) {
+            synchronized (ThreadSafeSingleton.class) {
+                safeSingleton = new ThreadSafeSingleton();
+            }
+        }
+        return safeSingleton;
+    }
+
     // Write your code here!
 }
